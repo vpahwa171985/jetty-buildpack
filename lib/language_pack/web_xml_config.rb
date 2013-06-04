b953e97e5860a8b59c15b792e39e46984475edf3
Jesse McConnell <jesse.mcconnell@gmail.com>
2013-06-04 15:23:12 -0500

    remove nokogiri

--- lib/language_pack/web_xml_config.rb
+++ lib/language_pack/web_xml_config.rb
@@ -1,5 +1,3 @@
-require "nokogiri"
-
 module LanguagePack
   class WebXmlConfig
     CONTEXT_CONFIG_LOCATION = "contextConfigLocation".freeze
@@ -9,7 +7,7 @@ module LanguagePack
     attr_reader :default_app_context_location, :context_params, :servlet_params, :default_servlet_context_locations, :prefix
 
     def initialize(web_xml, default_app_context_location, context_params, servlet_params,  default_servlet_context_locations={})
-      @parsed_xml = Nokogiri::XML(web_xml)
+      @parsed_xml = XmlWrapper.new(web_xml)
       @default_app_context_location = default_app_context_location
       @context_params = context_params
       @servlet_params = servlet_params
@@ -22,84 +20,95 @@ module LanguagePack
     end
 
     def configure_autostaging_context_param
-      context_config_location_node = @parsed_xml.xpath("//#{prefix}context-param[contains(normalize-space(#{prefix}param-name), normalize-space('#{CONTEXT_CONFIG_LOCATION}'))]").first
-      context_param = autostaging_param_value "#{prefix}context-param"
+      xpath = "//#{prefix}context-param"
+      context_config_location_node = @parsed_xml.xpath(xpath).find do |node|
+        @parsed_xml.first("#{node.xpath}/#{prefix}param-name").text.strip == CONTEXT_CONFIG_LOCATION
+      end
+
+      context_param = autostaging_param_value("//#{prefix}context-param")
       if context_config_location_node
-        update_param_value context_config_location_node, context_param
+        update_param_value("#{prefix}param-value", context_param, ' ', context_config_location_node)
       elsif default_app_context_location
-        add_param_node @parsed_xml.root, "context-param", CONTEXT_CONFIG_LOCATION, "#{default_app_context_location} #{context_param}"
+        add_param_node(path: @parsed_xml.root.xpath, node_name: "context-param", name: CONTEXT_CONFIG_LOCATION, value: "#{default_app_context_location} #{context_param}")
       end
     end
 
     def configure_springenv_context_param
-      context_param_node =  @parsed_xml.xpath("//#{prefix}context-param[contains(normalize-space(#{prefix}param-name), normalize-space('#{CONTEXT_INITIALIZER_CLASSES}'))]").first
-      if context_param_node
-        update_param_value context_param_node, context_params[:contextInitializerClasses], ", "
+      xpath = "//#{prefix}context-param[contains(#{prefix}param-name, '#{CONTEXT_INITIALIZER_CLASSES}')]"
+      node = @parsed_xml.first(xpath)
+
+      if node
+        update_param_value 'param-value', context_params[:contextInitializerClasses], ", ", node
       else
-        add_param_node @parsed_xml.root, "context-param", CONTEXT_INITIALIZER_CLASSES, context_params[:contextInitializerClasses]
+        add_param_node(path: @parsed_xml.root.xpath, node_name: "context-param", name: CONTEXT_INITIALIZER_CLASSES, value: context_params[:contextInitializerClasses])
       end
     end
 
     def configure_autostaging_servlet
-      dispatcher_servlet_nodes = @parsed_xml.xpath("//#{prefix}servlet[contains(normalize-space(#{prefix}servlet-class), normalize-space('#{servlet_params[:dispatcherServletClass]}'))]")
-      return unless dispatcher_servlet_nodes
+      dispatcher_servlet_nodes = @parsed_xml.xpath("//#{prefix}servlet[contains(#{prefix}servlet-class, normalize-space('#{servlet_params[:dispatcherServletClass]}'))]")
 
-      dispatcher_servlet_nodes.each do |dispatcher_servlet_node|
-        dispatcher_servlet_name = dispatcher_servlet_node.xpath("#{prefix}servlet-name").first.content.strip
-        init_param_node = dispatcher_servlet_node.xpath("#{prefix}init-param[contains(normalize-space(#{prefix}param-name), normalize-space('#{CONTEXT_CONFIG_LOCATION}'))]").first
-        init_param = autostaging_param_value "#{prefix}servlet/#{prefix}init-param"
-        if init_param_node
-          update_param_value init_param_node, init_param
-        else
-          init_param = "#{default_servlet_context_locations[dispatcher_servlet_name]} #{init_param}" if has_servlet_context_location?(dispatcher_servlet_name)
-          add_param_node dispatcher_servlet_node, "init-param", CONTEXT_CONFIG_LOCATION, init_param
-        end
+      dispatcher_servlet_nodes.each do |servlet_node|
+        create_or_update_init_param(servlet_node)
       end
     end
 
     private
-    def add_param_node(parent, node_name, name, value)
-      param_node = Nokogiri::XML::Node.new node_name, @parsed_xml
 
-      param_name_node = Nokogiri::XML::Node.new 'param-name', @parsed_xml
-      param_name_node.content = name
-      param_node.add_child param_name_node
+    def create_or_update_init_param(servlet_node)
+      init_param = autostaging_param_value("//#{prefix}servlet/#{prefix}init-param")
 
-      param_value_node = Nokogiri::XML::Node.new 'param-value', @parsed_xml
-      param_value_node.content = value
-      param_node.add_child param_value_node
+      xpath = "#{prefix}init-param[contains(#{prefix}param-name, normalize-space('#{CONTEXT_CONFIG_LOCATION}'))]"
+      init_param_node = @parsed_xml.xpath(xpath, servlet_node).first
 
-      parent.add_child param_node
-    end
-
-    def update_param_value(node, new_value, separator=" ")
-      value_node = node.xpath("#{prefix}param-value").first
-      old_value = value_node.content
-      return if old_value.split.include?(new_value) || old_value == ''
-
-      value_node.content += "#{separator}#{new_value}"
-    end
-
-    def autostaging_param_value(xpath)
-      contextClass = @parsed_xml.xpath("//#{xpath}[contains(normalize-space(#{prefix}param-name), normalize-space('contextClass'))]")
-      if context_params[:contextConfigLocationAnnotationConfig] && contextClass.xpath("#{prefix}param-value").text.strip == ANNOTATION_CONTEXT_CLASS
-        context_params[:contextConfigLocationAnnotationConfig]
+      if init_param_node
+        update_param_value 'param-value', init_param, " ", init_param_node
       else
-        context_params[:contextConfigLocation]
+        prepend = has_location?(servlet_node) ? "#{default_servlet_context_locations[servlet_name(servlet_node)]} " : ""
+        add_param_node(path: servlet_node.xpath, node_name: "init-param", name: CONTEXT_CONFIG_LOCATION, value: prepend + init_param)
       end
     end
 
+    def servlet_name(servlet_node)
+      @parsed_xml.xpath("#{prefix}servlet-name", servlet_node).map(&:text).map(&:strip).first
+    end
+
+    def add_param_node(opts)
+      parent = @parsed_xml.add_node(path: opts[:path], name: opts[:node_name])
+      @parsed_xml.add_node(name: "param-name", value: opts[:name], relative_node: parent)
+      @parsed_xml.add_node(name: "param-value", value: opts[:value], relative_node: parent)
+    end
+
+    # TODO: the passed in separator is not used in the split, is that supposed to be that way?
+    #       It could end up in a state like "a b c,e;f" if several separators are used
+    def update_param_value(path, new_value, separator=" ", node = nil)
+      @parsed_xml.update_node_text(path, node) do |text|
+        text ||= ''
+        if text.split.include?(new_value) || text == ''
+          text
+        else
+          "#{text}#{separator}#{new_value}"
+        end
+      end
+    end
+
+    def autostaging_param_value(xpath)
+      node = @parsed_xml.first("//#{xpath}[contains(#{prefix}param-name, 'contextClass')]/#{prefix}param-value")
+      key = node && node.text.strip == ANNOTATION_CONTEXT_CLASS ? :contextConfigLocationAnnotationConfig : :contextConfigLocation
+      context_params[key]
+    end
+
     def namespace_prefix
-      name_space = @parsed_xml.root.namespace
+      name_space = @parsed_xml.root.namespaces.first
       if name_space
-        name_space.prefix ? name_space.prefix : "xmlns:"
+        "#{name_space.first}:"
       else
         ''
       end
     end
 
-    def has_servlet_context_location?(dispatcher_servlet_name)
-      default_servlet_context_locations && default_servlet_context_locations[dispatcher_servlet_name]
+    def has_location?(servlet_node)
+      dispatcher_servlet_name = servlet_name(servlet_node)
+      !!(default_servlet_context_locations && default_servlet_context_locations[dispatcher_servlet_name])
     end
   end
 end
